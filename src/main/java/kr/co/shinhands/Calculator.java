4dce12bcb19e09e09698e1ca81dde2fa0d9c7e9d
Kyung <xoelxowl92@gmail.com>
2024-04-28 18:10:25 +0900

    no message

---
+++ src/main/java/kr/co/shinhands/Calculator.java
@@ -0,0 +1,20 @@
+package kr.co.shinhands;
+
+public class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    int divide(int a, int b) {
+        return a / b;
+    }
+
+
+}
